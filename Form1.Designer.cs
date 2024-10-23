3e1f78c6efc7d25fa8dae26d172d95e7d9f79dd2
Your Name <ABDULSELAM ELAHMED>
2024-10-23 14:23:56 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace flapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.player = new System.Windows.Forms.Label();
+            this.timer1 = new System.Timers.Timer();
+            this.enemy = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.player.Location = new System.Drawing.Point(46, 183);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(28, 22);
+            this.player.TabIndex = 1;
+            this.player.Text = "🛸";
+            this.player.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 16D;
+            this.timer1.SynchronizingObject = this;
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
+            // 
+            // enemy
+            // 
+            this.enemy.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enemy.ForeColor = System.Drawing.Color.Red;
+            this.enemy.Location = new System.Drawing.Point(639, 161);
+            this.enemy.Name = "enemy";
+            this.enemy.Size = new System.Drawing.Size(18, 18);
+            this.enemy.TabIndex = 2;
+            this.enemy.Text = "🛸";
+            this.enemy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.enemy);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label enemy;
+
+        private System.Timers.Timer timer1;
+
+        private System.Windows.Forms.Label player;
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using Timer = System.Timers.Timer;
+
+namespace flapp
+{
+    public partial class Form1 : Form
+    {
+        private int gravity = 1;
+        private int speed = 6;
+        private int time;
+        private bool jumping,alive;
+        public Form1()
+        {
+            
+            InitializeComponent();
+         
+          
+        }
+        
+        
+      
+
+        
+        
+       
+
+        private bool Crashing(Label p,Label c )
+        {
+            if (p.Bounds.IntersectsWith(c.Bounds))
+            {
+                Console.WriteLine(@"Crashing");
+                return true;
+            }
+            return false;
+        }
+
+
+        private void Jump(int speed)
+        {
+            
+               player.Top -= speed*30;
+            time = 0;
+        }
+
+        private void Gravity()
+        {
+            if (player.Location.Y < 395 )
+            {
+                player.Top += time*gravity/5;
+                if (time * gravity  > 395)
+                    time --;
+            }
+            
+            if (player.Location.Y < 1)
+            {
+                player.Location = new Point(player.Location.X, 0);
+            }
+
+            if (jumping)
+            {
+               Jump(1);
+                jumping = false;
+            }
+                
+        }
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Column(enemy);
+          Gravity();
+         if (Crashing(player,enemy))
+             timer1.Stop();
+        time++;
+        }
+
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+                jumping = true;
+                
+           
+        }
+
+        private void Column(Label enemy)
+        {
+            Random random = new Random();
+            enemy.Left -= 20;
+            if (enemy.Left < 0)
+            {
+                enemy.Left = 900;
+                enemy.Top = random.Next(5, 390);
+            }
+                
+            
+        }
+       
+
+    }
+}
