83ecbd08e6b9fbf0869eb92be5959e7cf34d3dab
Your Name <ABDULSELAM ELAHMED>
2024-10-23 19:42:37 +0300

    update

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,22 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.player = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.timer1 = new System.Timers.Timer();
-            this.enemy = new System.Windows.Forms.Label();
+            this.fish = new System.Windows.Forms.PictureBox();
+            this.coloCHECKER = new System.Windows.Forms.PictureBox();
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.colo = new System.Windows.Forms.PictureBox();
+            this.coloCHECKER1 = new System.Windows.Forms.PictureBox();
+            this.colo1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloCHECKER)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloCHECKER1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colo1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // player
-            // 
-            this.player.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.player.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.player.Location = new System.Drawing.Point(46, 183);
-            this.player.Name = "player";
-            this.player.Size = new System.Drawing.Size(28, 22);
-            this.player.TabIndex = 1;
-            this.player.Text = "🛸";
-            this.player.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timer1
             // 
@@ -52,38 +51,116 @@
             this.timer1.SynchronizingObject = this;
             this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
             // 
-            // enemy
+            // fish
             // 
-            this.enemy.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.enemy.ForeColor = System.Drawing.Color.Red;
-            this.enemy.Location = new System.Drawing.Point(639, 161);
-            this.enemy.Name = "enemy";
-            this.enemy.Size = new System.Drawing.Size(18, 18);
-            this.enemy.TabIndex = 2;
-            this.enemy.Text = "🛸";
-            this.enemy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.fish.BackColor = System.Drawing.Color.Transparent;
+            this.fish.Image = ((System.Drawing.Image)(resources.GetObject("fish.Image")));
+            this.fish.InitialImage = null;
+            this.fish.Location = new System.Drawing.Point(22, 118);
+            this.fish.Name = "fish";
+            this.fish.Size = new System.Drawing.Size(46, 32);
+            this.fish.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.fish.TabIndex = 3;
+            this.fish.TabStop = false;
+            this.fish.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // coloCHECKER
+            // 
+            this.coloCHECKER.BackColor = System.Drawing.Color.Transparent;
+            this.coloCHECKER.InitialImage = null;
+            this.coloCHECKER.Location = new System.Drawing.Point(766, -8);
+            this.coloCHECKER.Name = "coloCHECKER";
+            this.coloCHECKER.Size = new System.Drawing.Size(11, 466);
+            this.coloCHECKER.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coloCHECKER.TabIndex = 5;
+            this.coloCHECKER.TabStop = false;
+            this.coloCHECKER.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.BackColor = System.Drawing.Color.Transparent;
+            this.scorelabel.Font = new System.Drawing.Font("Segoe MDL2 Assets", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scorelabel.Location = new System.Drawing.Point(12, 26);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(95, 27);
+            this.scorelabel.TabIndex = 6;
+            this.scorelabel.Text = "Score : 0";
+            // 
+            // colo
+            // 
+            this.colo.BackColor = System.Drawing.Color.Transparent;
+            this.colo.Image = ((System.Drawing.Image)(resources.GetObject("colo.Image")));
+            this.colo.InitialImage = null;
+            this.colo.Location = new System.Drawing.Point(750, 392);
+            this.colo.Name = "colo";
+            this.colo.Size = new System.Drawing.Size(65, 272);
+            this.colo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.colo.TabIndex = 7;
+            this.colo.TabStop = false;
+            this.colo.Click += new System.EventHandler(this.colo_Click);
+            // 
+            // coloCHECKER1
+            // 
+            this.coloCHECKER1.BackColor = System.Drawing.Color.Transparent;
+            this.coloCHECKER1.InitialImage = null;
+            this.coloCHECKER1.Location = new System.Drawing.Point(381, -5);
+            this.coloCHECKER1.Name = "coloCHECKER1";
+            this.coloCHECKER1.Size = new System.Drawing.Size(10, 463);
+            this.coloCHECKER1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.coloCHECKER1.TabIndex = 9;
+            this.coloCHECKER1.TabStop = false;
+            // 
+            // colo1
+            // 
+            this.colo1.BackColor = System.Drawing.Color.Transparent;
+            this.colo1.Image = ((System.Drawing.Image)(resources.GetObject("colo1.Image")));
+            this.colo1.InitialImage = null;
+            this.colo1.Location = new System.Drawing.Point(354, -282);
+            this.colo1.Name = "colo1";
+            this.colo1.Size = new System.Drawing.Size(59, 271);
+            this.colo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.colo1.TabIndex = 10;
+            this.colo1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.enemy);
-            this.Controls.Add(this.player);
+            this.Controls.Add(this.colo1);
+            this.Controls.Add(this.coloCHECKER1);
+            this.Controls.Add(this.colo);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.coloCHECKER);
+            this.Controls.Add(this.fish);
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fish)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloCHECKER)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.coloCHECKER1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.colo1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        private System.Windows.Forms.Label enemy;
+        private System.Windows.Forms.PictureBox coloCHECKER1;
 
-        private System.Timers.Timer timer1;
+        private System.Windows.Forms.PictureBox colo1;
 
-        private System.Windows.Forms.Label player;
+        public System.Timers.Timer timer1;
 
         #endregion
+
+        private System.Windows.Forms.PictureBox fish;
+        private System.Windows.Forms.PictureBox coloCHECKER;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.PictureBox colo;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,18 +16,18 @@ namespace flapp
 {
     public partial class Form1 : Form
     {
-        private int gravity = 1;
+        private int gravity = 4;
         private int speed = 6;
         private int time;
-        private bool jumping,alive;
+        private bool jumping,alive=true;
+        private int score = 0;
         public Form1()
         {
             
             InitializeComponent();
-         
-          
         }
-        
+
+    
         
       
 
@@ -35,77 +35,142 @@ namespace flapp
         
        
 
-        private bool Crashing(Label p,Label c )
+        private bool Crashing(PictureBox p,PictureBox c )
         {
             if (p.Bounds.IntersectsWith(c.Bounds))
             {
-                Console.WriteLine(@"Crashing");
+                
                 return true;
             }
             return false;
         }
-
-
-        private void Jump(int speed)
-        {
-            
-               player.Top -= speed*30;
-            time = 0;
-        }
-
         private void Gravity()
         {
-            if (player.Location.Y < 395 )
+            if (fish.Location.Y < 395 )
             {
-                player.Top += time*gravity/5;
+                fish.Top += time*gravity/5;
                 if (time * gravity  > 395)
                     time --;
             }
             
-            if (player.Location.Y < 1)
+            if (fish.Location.Y < 1)
             {
-                player.Location = new Point(player.Location.X, 0);
+                fish.Location = new Point(fish.Location.X, 0);
             }
 
             if (jumping)
             {
-               Jump(1);
+                Jump(1);
                 jumping = false;
             }
-                
+
         }
 
-        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        private void Jump(int speed)
         {
-            Column(enemy);
-          Gravity();
-         if (Crashing(player,enemy))
-             timer1.Stop();
-        time++;
+            for (int i = 0; i < speed; i++)
+            {
+                fish.Top -= speed*30;
+            }
+               
+            time = 0;
         }
+
+       
+
+        private void timer1_Elapsed(object sender, ElapsedEventArgs e)
+        {  
+            
+            Gravity();
+            UpColumn(colo,coloCHECKER);
+            DownColumn(colo1,coloCHECKER1);
+         
+         if (Crashing(fish,colo)||Crashing(fish,colo1))
+             timer1.Stop(); 
+         time++;
+        }
+        
 
 
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
             if(e.KeyCode == Keys.Space)
                 jumping = true;
-                
+               
            
         }
 
-        private void Column(Label enemy)
+        private void pictureBox1_Click(object sender, EventArgs e)
         {
-            Random random = new Random();
-            enemy.Left -= 20;
-            if (enemy.Left < 0)
+
+        }
+
+        private void enemy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void ScoreChecker( PictureBox coloCHECKER)
+        {
+            if (fish.Bounds.IntersectsWith(coloCHECKER.Bounds))
             {
-                enemy.Left = 900;
-                enemy.Top = random.Next(5, 390);
+                score++;
+                
+            }
+scorelabel.Text = "Score : " + score/3;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void colo_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void UpColumn(PictureBox colomn,PictureBox scorecheck)
+        {
+            ScoreChecker(scorecheck);
+            Random random = new Random();
+            colomn.Left -= 20;
+            scorecheck.Left -= 20;
+            if (colomn.Left < 0)
+            {
+                int rnd=random.Next(249, 350);
+                colomn.Left = 800;
+                scorecheck.Left = 826;
+             
+                colomn.Top = rnd;
             }
                 
             
         }
-       
+        private void DownColumn(PictureBox colomn ,PictureBox scorecheck)
+        {
+            
+            ScoreChecker(scorecheck);
+            Random random = new Random();
+            colomn.Left -= 20;
+            scorecheck.Left -= 20;
+            if (colomn.Left < 0)
+            {
+                int rnd=random.Next(0, 209);
+                colomn.Left = 800;
+                scorecheck.Left = 826;
+             
+                colomn.Top = -rnd;
+            }
+                
+            
+        }
 
+
+     
     }
 }
